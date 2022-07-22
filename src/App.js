e137fc8010589f339f52f3e79c9a456c2c8099b2
kimsohee <726sohee@gs.anyang.ac.kr>
2022-07-22 23:22:44 +0900

    feat : add edit page sevice

--- src/App.js
+++ src/App.js
@@ -110,9 +110,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/new" element={<New />} />
-              <Route path="/edit" element={<Edit />} />
-              <Route path="/diary/:1" element={<Diary />} />
-              <Route path="/diary" element={<Diary />} />
+              <Route path="/edit/:id" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
             </Routes>
           </div>
         </BrowserRouter>
